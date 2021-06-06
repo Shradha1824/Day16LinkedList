3c7e982d98d392ff6284b23568e9d1441c3334d7
Shradha1824 <shradhabj1824@gmail.com>
2021-06-06 10:29:20 -1200

    Added file to Append Given nodes

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -14,12 +14,46 @@ public class SinglyLinkedList {
 		}
 	}
 
-	public static void main(String[] args){
+	public void Append(ListNode previous, int data) {
+		if(previous == null) {
+			System.out.println("The Given previous node cannot be null");
+			return;
+		}
+		
+		ListNode newNode = new ListNode(data);
+		newNode.next = previous.next;
+		previous.next = newNode;
+	}
+
+	public void display() {
+		ListNode current = head;
+		while(current != null) {
+			System.out.print(current.data + " --> ");
+			current = current.next;
+		}
+		System.out.print("null");
+	}
+
+
+	public static void createLikedList(String[] args){
 	//Create A LinkedList
 
-	ListNode head = new ListNode(70);
+	ListNode head = new ListNode(56);
 	ListNode second = new ListNode(30);
-	ListNode third = new ListNode(56);
+	ListNode third = new ListNode(70);
 
+	head = first;
+	first.next = second;
+	second.next = third;
+
+	public static void main(String[] args){
+	
+	    SinglyLinkedList sll = new SinglyLinkedList();
+	    sll.display();
+	    System.out.println();
+            sll.Append(first, 30);
+	    sll.display();
+	    sll.Append(second, 70);
+	    sll.display();
 	}
 }
