97849ffe1347597f0ae50e9ef12c7dcb64ad5298
Shradha1824 <shradhabj1824@gmail.com>
2021-06-06 09:51:37 -1200

    First commit

---
+++ SinglyLinkedList.java
@@ -0,0 +1,25 @@
+package linkedlist;
+
+public class SinglyLinkedList {
+	
+	private ListNode head;
+
+	private static class ListNode {
+		private int data; // Can be a generic type
+		private ListNode next; // Reference to next ListNode in list
+
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	public static void main(String[] args){
+	//Create A LinkedList
+
+	ListNode head = new ListNode(56);
+	ListNode second = new ListNode(30);
+	ListNode third = new ListNode(70);
+
+	}
+}
