96f85ff85c496c04cc9638f0f4f474d45d3a8847
Shradha1824 <shradhabj1824@gmail.com>
2021-06-06 13:17:46 -1200

    Added file to print in sorted manner

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -122,23 +122,37 @@ public class SinglyLinkedList {
 		return null;
 	}
 
+	public ListNode insertInSortedList(int value) {
+		ListNode newNode = new ListNode(value);
+
+		if(head == null) {
+			return newNode;
+		}
+
+		ListNode current = head;
+		ListNode temp = null;
+
+		while (current != null && current.data < newNode.data) {
+			temp = current;
+			current = current.next;
+		}
+
+		newNode.next = current;
+		temp.next = newNode;
+		return head;
+	}
+
 	public static void main(String[] args){
 	//Create A LinkedList
 	
 		SinglyLinkedList sll = new SinglyLinkedList();
 
-		ListNode head = new ListNode(56);
-		ListNode second = new ListNode(30);
-		ListNode third = new ListNode(70);
+		sll.insertFirst(70);
+		sll.insertFirst(56);
+		sll.insertFirst(30);
 
-		head = first;
-		first.next = second;
-		second.next = third;
+		sll.insertInSortedList(40);
+		sll.display();
 
-		third = sll.insertAtPosition(head, 3, 40);
-		sll.display();
-		third = sll.delete(3);
-		System.out.println("\n");
-		sll.display();
 	}
 }
